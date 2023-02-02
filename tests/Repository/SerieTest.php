09bc2ec852975bc39272324fe9daa8bff61fc687
Caliendo Julien <caliendo@hotmail.fr>
2023-02-02 21:12:54 +0100

    RepoTests 1/2

--- tests/Repository/SerieTest.php
+++ tests/Repository/SerieTest.php
@@ -2,12 +2,33 @@
 
 namespace App\Tests\Repository;
 
+use App\Entity\Serie;
+use App\Repository\SerieRepository;
 use PHPUnit\Framework\TestCase;
 
 class SerieTest extends TestCase
 {
-    public function testSomething(): void
+    public function testAvgSF(): void
     {
-        $this->assertTrue(true);
+        $maintenant = new \DateTime();
+        $serie = (new Serie())
+            ->setName('GoT')
+            ->setGenres('')
+            ->setOverview('')
+            ->setStatus('')
+            ->setVote(8)
+            ->setPopularity(4)
+            ->setFirstAirDate(null)
+            ->setLastAirDate(null)
+            ->setBackdrop('')
+            ->setPoster('')
+            ->setTmdbid(123456789)
+            ->setDateCreated($maintenant)
+            ->setDateModified(null);
+        $serieRepo = $this->createMock(SerieRepository::class);
+        $serieRepo->expects($this->any())
+            ->method('find')
+            ->willReturn($serie);
+
     }
 }
